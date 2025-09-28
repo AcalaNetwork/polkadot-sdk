42ec1d52c203a6816e4d01ea01eb987127e7234e
Bryan Chen <xlchen1291@gmail.com>
2025-09-29 11:58:22 +1300

    setup pallet savings

---
+++ substrate/frame/honzon/savings/src/lib.rs
@@ -0,0 +1,34 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config + pallet_asset_rewards::Config {
+        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        // Placeholder for future events
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        // Placeholder for future errors
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        // Placeholder for future extrinsics
+    }
+}
